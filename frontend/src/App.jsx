07daed20ef8d99da60394043db969a38ca7d8274
ez-sourav <souravb2003@gmail.com>
2026-03-08 11:25:45 +0530

    add AuthProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from "react-router"
 import { router } from "./app.routes.jsx"
-
+import { AuthProvider } from "./features/auth/auth.context.js"
 
 const App = () => {
   return (
+    <AuthProvider>
     <RouterProvider router={router}/>
+    </AuthProvider>
   )
 }
 
