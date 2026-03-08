f04fb1e2500336d61f7a8409254d63502d944c86
ez-sourav <souravb2003@gmail.com>
2026-03-08 16:52:15 +0530

    implement  cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express(); 
 const authRouter = require('./routes/auth.routes')
 const cookieParser = require('cookie-parser');
+const cors = require('cors')
 
 app.use(cookieParser())
 
 app.use(express.json())
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 app.use('/api/auth',authRouter)
 
