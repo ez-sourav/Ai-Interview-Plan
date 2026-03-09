6a5c5f1c4275190045ad972082df77c3c69efcd2
ez-sourav <souravb2003@gmail.com>
2026-03-09 19:56:28 +0530

    change responemessgae in getMeController

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -163,9 +163,11 @@ async function getMeController(req,res){
     const user = await userModel.findById(req.user.id)
     res.status(200).json({
         message:"User details fetched successfully",
-        id:user._id,
-        username:user.username,
-        email:user.email
+        user:{
+            id:user._id,
+            username:user.username,
+            email:user.email
+        }
     })
 }
 module.exports = { registerUserController, loginUserController, logoutUserController, getMeController }
