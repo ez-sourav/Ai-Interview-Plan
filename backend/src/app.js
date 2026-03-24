8677815fc68eb9e43ac59b60b9fa72c52be1a443
ez-sourav <souravb2003@gmail.com>
2026-03-24 15:26:16 +0530

    change URL in cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,8 @@ app.use(cookieParser())
 app.use(express.json())
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    // origin:"http://localhost:5173",
+    origin:"https://interviewplanner.vercel.app",
     credentials:true
 }))
 
